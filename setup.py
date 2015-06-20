84056868a3015c09fda2874d466fcfdbec8bbb30
moyiz <moyiz@users.noreply.github.com>
2015-06-20 19:27:15 +0300

    Added a license and a simple setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sogol",
+    vesion="0.1",
+    packages=find_packages(),
+    scripts=['sogolife.py'],
+    author="moyiz",
+    author_email="",
+    description="Sound of game of life.",
+    license="BSD",
+    keywords="gol sol game of life sound of life",
+    url=""
+)
