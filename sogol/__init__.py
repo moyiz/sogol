dd8db10f1168fbeb84676887237324f9e69da15a
moyiz <8603313+moyiz@users.noreply.github.com>
2021-10-13 08:41:13 +0300

    Refactor wave generation, data playing, optimize board generation, add main script

--- sogol/__init__.py
+++ sogol/__init__.py
@@ -1,1 +1,50 @@
-from .sogol import SoundOfLife
+"""
+Sound of Game of Life.
+Listen to an audio interpretation of each generation.
+
+Usage:
+    sogol [--max-column=X] [--max-row=Y] [--cells=N] [-d DELAY] [-w WAVE]
+    sogol (--from-json=PATH|--from-string=BOARD) [-d DELAY] [-w WAVE]
+
+Options:
+    -x NUM, --max-column=NUM   Maximum column of randomly generated cells [default: 3]
+    -y NUM, --max-row=NUM      Maximum row of randomly generated cells [default: 3]
+    -c CELLS, --cells=CELLS    Maximum number of cells to randomly generate [default: 7]
+    -j FILE, --from-json=FILE  Load board from JSON file
+    -d DELAY, --delay=DELAY    Delay in seconds between each generation [default: 0.5]
+    -w (sine|square|sawtooth)  Select a wave to generate [default: sine]
+"""
+import json
+import sys
+import time
+
+from docopt import docopt
+
+from sogol.game import BoardBuilder, SoundOfLife
+from sogol.sound import SawtoothWave, SineWave, SquareWave
+
+wave_map = {"sine": SineWave, "square": SquareWave, "sawtooth": SawtoothWave}
+
+
+def main():
+    args = docopt(__doc__)
+    print(args)
+    if args["--from-json"]:
+        with open(args["--from-json"]) as fp:
+            board = tuple(tuple(cell) for cell in json.load(fp))
+    elif args["--from-string"]:
+        board = tuple(tuple(cell) for cell in json.loads(args["--from-string"]))
+    else:
+        board = BoardBuilder.generate_random_board(
+            max_column=int(args["--max-column"]),
+            max_row=int(args["--max-row"]),
+            max_cells=int(args["--cells"]),
+        )
+
+    delay = float(args["--delay"])
+    try:
+        while board:
+            board = SoundOfLife.do_turn(board, wave=wave_map.get(args["-w"]))
+            time.sleep(delay)
+    except KeyboardInterrupt:
+        sys.exit(1)
--- sogol/game.py
+++ sogol/game.py
@@ -1,129 +1,96 @@
-import random
+from functools import lru_cache
+from itertools import product
+from random import randint
+from typing import Dict, Tuple
+
+from .sound import Channel, Player, SineWave, Wave
+
+Cell = Tuple[int, int]
+Board = Tuple[Cell]
+ExpandedBoard = Dict[Cell, bool]
 
 
-class Game(object):
-    def __init__(self):
-        self.board = Board()
-
-    def fill_random(self, max_x, max_y, cells):
-        """
-        Fills random cells between the range specified.
-
-        :param max_x: Maximum x range
-        :type max_x: int
-        :param max_y: Maximum y range
-        :type max_y: int
-        :param cells: The number of cells to generate
-        :type cells: int
-        :return: None
-        """
-        for _ in range(cells):
-            self.board.set_cell(random.randint(0, max_x), random.randint(0, max_y))
-
-    def do_turn(self):
-        """
-        Does one turn in the game, and returns the current
-        board state
-        :return: The current board
-        :type: Board
-        """
-        new_board = Board()
-        for x, y in self.board.get_cells():
-            if self.board.get_cell(x, y) and self.board.count_living_neigh(x, y) in [
-                2,
-                3,
-            ]:
-                new_board.set_cell(x, y)
-            elif (
-                not self.board.get_cell(x, y)
-                and self.board.count_living_neigh(x, y) == 3
-            ):
-                new_board.set_cell(x, y)
-        self.board = new_board
-        return new_board
-
-
-class Board(object):
-    def __init__(self):
-        self._board = {}
-
-    def get_cells(self):
-        """
-        Returns a list of relevant cells (living and their neighbours).
-
-        :return: List(int, int)
-        """
-        cells = list(self._board.keys())
-        for x, y in self._board.keys():
-            cells += Board.get_neighs(x, y)
-        return cells
-
-    def get_living_cells(self):
-        """
-        Returns a list of coords of the living cells in the board
-        :return: List(int, int)
-        """
-        return self._board.keys()
-
-    def set_cell(self, x, y, alive=True):
-        """
-        Sets the specified cell.
-
-        :param x: x coord of the cell
-        :type x: int
-        :param y: y coord of the cell
-        :type y: int
-        :param alive: status of cell
-        :type alive: bool
-        :return: None
-        """
-        self._board[(x, y)] = alive
-
-    def get_cell(self, x, y):
-        """
-        Returns the status of a cell.
-
-        :return: True for a living cell or False for a dead one.
-        :rtype: bool
-        """
-        if (x, y) not in self._board:
-            return False
-        return True
+class BoardBuilder:
+    @staticmethod
+    def generate_random_board(max_column: int, max_row: int, max_cells: int) -> Board:
+        return tuple(
+            set((randint(0, max_column), randint(0, max_row)) for _ in range(max_cells))
+        )
 
     @staticmethod
-    def get_neighs(x, y):
-        """
-        Returns a list of the current cell's neighbours.
+    def from_lexicon(board: str):
+        raise NotImplementedError
 
-        :param x: x coord of the cell
-        :type x: int
-        :param y: y coord of the cell
-        :type y: int
-        :return: List(int, int)
-        """
-        return [
-            (i, j)
-            for i in range(x - 1, x + 2)
-            for j in range(y - 1, y + 2)
-            if (i, j) != (x, y)
-        ]
 
-    def count_living_neigh(self, x, y):
+class GameOfLife:
+    @classmethod
+    def do_turn(cls, living_cells: Board) -> Board:
         """
-        Returns the number of living neighbours of the
-        specified cell.
+        Returns the next state of the board.
+        """
+        expanded_board = cls._get_expanded_board(living_cells)
+        next_generation_set = {
+            cell
+            for cell, state in expanded_board.items()
+            if cls._living_neighbours(living_cells, cell) == 3
+            or expanded_board.get(cell)
+            and cls._living_neighbours(living_cells, cell) == 2
+        }
+        return tuple(next_generation_set)
 
-        :param x: x coord of the cell
-        :type x: int
-        :param y: y coord of the cell
-        :type y: int
-        :return: int
-        """
-        return len(
-            [
-                1
-                for i in range(x - 1, x + 2)
-                for j in range(y - 1, y + 2)
-                if self.get_cell(i, j) and (i, j) != (x, y)
-            ]
+    @classmethod
+    @lru_cache()
+    def _living_neighbours(cls, living_cells: Board, cell: Cell) -> int:
+        x, y = cell
+        neighbours = (
+            neigh
+            for neigh in product(range(x - 1, x + 2), range(y - 1, y + 2))
+            if cell != neigh
         )
+        return len([neigh for neigh in neighbours if neigh in living_cells])
+
+    @staticmethod
+    def _get_expanded_board(living_cells: Board) -> ExpandedBoard:
+        """
+        Get a board that includes the neighbours of the given cells.
+        """
+        board = {cell: True for cell in living_cells}
+        return {
+            cell: board.get(cell, False)
+            for (x, y) in board.keys()
+            for cell in product(range(x - 1, x + 2), range(y - 1, y + 2))
+        }
+
+
+class SoundOfLife(GameOfLife):
+    """
+    A game of sound.
+    Creates a game, generates & plays generation of each turn.
+    """
+
+    @classmethod
+    def do_turn(cls, living_cells: Board, wave: Wave = SineWave) -> Board:
+        """
+        Plays the given board and returns the next generation.
+        """
+        cls.generate_sound(living_cells, wave).play()
+        return super().do_turn(living_cells)
+
+    @classmethod
+    def generate_sound(cls, board: Board, wave: Wave) -> Player:
+        sounds = [
+            wave(
+                freq=440 + (12 ** 0.5) ** x,
+                rate=44100,
+                amp=cls._normalize(y),
+            ).duration(0.2)
+            for (x, y) in board
+        ]
+        lower_mid = len(sounds) // 2
+        c1 = Channel(sounds[:lower_mid])
+        c2 = Channel(sounds[lower_mid:])
+        return Player([c1.generator, c2.generator], 2)
+
+    @staticmethod
+    def _normalize(x, low=-1, high=1) -> float:
+        return 0 if x == 0 else (high - low) / x
